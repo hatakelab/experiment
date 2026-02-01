cce6a115c6d0873c92e248416f4219a0c3352688
hatakelab <htkr1126@icloud.com>
2026-02-01 11:34:52 +0900

    Add setsubun fn

--- src/lib.rs
+++ src/lib.rs
@@ -2,14 +2,26 @@ fn append_s(s: &str) -> String {
     format!("{}s", s)
 }
 
+fn setsubun(s: &str) -> String {
+    let t: Vec<&str> = s.to_string().slipt_whitespace().collect();
+    let n = t[0].parse().unwrap();
+    let mut k = t[1].parse().unwrap();
+    let ans = -1;
+    while k > 0 {
+        ans += 1;
+        k -= (n + ans);
+    }
+    format!("{}", ans)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn it_works() {
-        assert_eq!(append_s("http"), "https".to_string());
-        assert_eq!(append_s("append"), "appends".to_string());
-        assert_eq!(append_s("beginner"), "beginners".to_string());
+        assert_eq!(setsubun("4 43"), "6".to_string());
+        assert_eq!(setsubun("100000000 100000000"), "0".to_string());
+        assert_eq!(setsubun("1234 12345678"), "3886".to_string());
     }
 }
